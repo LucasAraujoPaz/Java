0a3a2a3e178f8ee57b22b114759598c9a9b74cc9
Lucas Paz <76001266+LucasAraujoPaz@users.noreply.github.com>
2020-12-16 20:58:42 -0300

    Add files via upload

---
+++ NewtonsMethod/NewtonsMethod.java
@@ -0,0 +1,50 @@
+package newtonsmethod;
+
+/** Insert values of x and n into the main method to obtain the n-root of x.
+ * Newton's method will not always work in any situation, be careful with large numbers.
+ * @author Lucas Paz*/
+public class NewtonsMethod {
+    
+    public static double NewtonsMethod(double x, long n){
+        double previous = 1; //previous == X[n]
+        double next = previous; //next == X[n+1]
+        double f = Math.pow(previous, n) - x; //function on X[n]
+        double fslope = n*(Math.pow(previous, (n-1))); //slope on X[n]
+
+        if (x < 0 || n <=0 ) {System.err.println("error"); return 0;}
+        else if (x == 0) return 0;
+        else if (n == 1 || x == 1) return x;
+        
+        int repetitions = 100;
+        
+        System.out.println("x[1] = " + previous + "\n");
+
+        for (int i = 0; i < repetitions; i++){
+            System.out.println("x[" + (i+1) + "] = " + previous);
+            System.out.println("f(x[" + (i+1) + "]) = " + f);
+            System.out.println("f'(x[" + (i+1) + "]) = " + fslope);
+            System.out.print("-> ");
+          
+            next = previous - (f/fslope);
+            
+            System.out.println("x[" + (i+2) + "] = " + next + "\n");
+
+            if (Math.abs(next - previous) < 0.000000000000001) break; //precision
+
+            f = Math.pow(next, n) - x; //function on X[n]
+            fslope = n*(Math.pow(next, (n-1))); //slope on X[n]
+            previous = next;
+        }
+        
+        return next;
+    }
+
+    
+    public static void main(String[] args) {
+        //n root of x
+        double x = 2;
+        long n = 2;
+        
+        System.out.println("result = " + NewtonsMethod(x, n));
+    }
+}
---
+++ NewtonsMethod/readme.txt
@@ -0,0 +1,7 @@
+Newton's Method is a java class which finds the n-root of x via that mathematical method.
+
+Insert values of x and n into the main method to obtain the n-root of x.
+
+Newton's method will not always work in any situation, be careful with large numbers.
+
+@author Lucas Paz.
