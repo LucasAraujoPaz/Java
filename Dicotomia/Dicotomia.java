0a3e7990cd14e55cb05f21ccdb6e7dc0e5760a3a
Lucas Paz <76001266+LucasAraujoPaz@users.noreply.github.com>
2020-12-16 23:51:51 -0300

    Add files via upload

---
+++ Dicotomia/Dicotomia.java
@@ -0,0 +1,57 @@
+package dicotomia;
+
+/**Dichotomy method (Bisection method)
+ * Insert values of x and n into the main method to obtain the n-root of x through the Dichotomy method.
+ *
+ * @author Lucas Paz*/
+public class Dicotomia {
+    
+    public static double dicotomia(double x, long n){
+        double result;
+        double max = (x < 1)? 1 : x; //superior limit
+        double min = 0; //inferior limit
+        double medium = (max+min)/2; //medium
+        
+        if (x < 0 || n <=0 ) {System.out.println("error"); return -1;}
+        else if (x == 0) return 0;
+        else if (n == 1 || x == 1) return x;
+        
+        int repetitions = 100;
+        
+        for (int i = 0; i < repetitions; i++) {
+            System.out.println(i+1);
+            System.out.println("min = " + min);            
+            System.out.println("max = " + max);
+            System.out.println("medium = " + medium);
+            System.out.print("-> ");
+            
+            double power = Math.pow(medium, n);
+            
+            if (power < x){
+                min = medium;
+                System.out.println("new min = " + min);
+            } else if (power == x) return medium;
+            else { // > x
+                max = medium;
+                System.out.println("new max = " + max);
+            }
+            medium = (max+min)/2;
+            System.out.println();
+            if (max - min < 0.000000000000001) break; //precision
+        }
+        System.out.println("precision range: between " + min + " and " + max);
+        System.out.println("margin of error = +- " + (max - medium));
+
+        result = medium;
+        return result;
+    }
+
+    
+    public static void main(String[] args) {
+        //n square of x
+        double x = 239874537;
+        long n = 275847584;
+        
+        System.out.println("\n" + "result = " + dicotomia(x, n));
+    }
+}
---
+++ Dicotomia/readme.txt
@@ -0,0 +1,15 @@
+Dichotomy method (Bisection method)
+
+Insert values of x and n into the main method to obtain the n-root of x through the Dichotomy method.
+
+The dichotomy method divides the intervals by half repeatedly in order to diminish the interval wherein the result is.
+
+---
+
+Método da dicotomia (Método da Bissecção)
+
+Insira valores de x e n no método main para obter a raiz-n de x via método da dicotomia.
+
+O método da dicotomia divide sucessivamente os intervalos pela metade para diminuir o intervalo onde se encontra a resposta.
+
+@author Lucas Paz*/
